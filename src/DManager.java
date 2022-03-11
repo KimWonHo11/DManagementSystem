e50a8dc695d5a3c88ac4b16042d3ca1bc7e83c43
KimWonHo11 <dnjsgh1707@naver.com>
2022-03-11 23:27:34 +0900

    added menu

--- src/DManager.java
+++ src/DManager.java
@@ -1,7 +1,11 @@
 
 public class DManager {
 	public static void main(String[] args) {
-           System.out.println("Hello World!");
+           System.out.println("1. Add Students");
+           System.out.println("2. Delete Students");
+           System.out.println("3. Edit Students");
+           System.out.println("4. View Students");
+           System.out.println("5. Exit");
 	}
 
 }
