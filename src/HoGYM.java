e7e1681574f20d2d5998dc6195ad3c0079a6e4e9
KimWonHo11 <dnjsgh1707@naver.com>
2022-03-13 19:24:49 +0900

    Added HoGYM

---
+++ src/HoGYM.java
@@ -0,0 +1,11 @@
+
+public class HoGYM {
+	public static void main(String[] args) {
+		
+		System.out.println("1. Add member");
+		System.out.println("2. Delete member");
+		System.out.println("3. Edit member");
+		System.out.println("4. View member");
+		System.out.println("5. Exit");
+	}
+}
